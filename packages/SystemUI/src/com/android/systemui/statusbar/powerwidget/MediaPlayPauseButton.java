a5daf6a4c35db87ac5fc312157d03a38ca86c7d4
Victor Nicolae <ihlades@gmail.com>
2011-02-13 20:00:17 +0200

    SystemUI: Cheap fix for the music controls. Update them everytime music playback is changed [Music app]

--- packages/SystemUI/src/com/android/systemui/statusbar/powerwidget/MediaPlayPauseButton.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/powerwidget/MediaPlayPauseButton.java
@@ -2,18 +2,30 @@ package com.android.systemui.statusbar.powerwidget;
 
 import com.android.systemui.R;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.AudioManager;
+import android.os.Handler;
 import android.view.KeyEvent;
 
 public class MediaPlayPauseButton extends MediaKeyEventButton {
-    public MediaPlayPauseButton() { mType = BUTTON_MEDIA_PLAY_PAUSE; }
 
     private static final int MEDIA_STATE_UNKNOWN  = -1;
     private static final int MEDIA_STATE_INACTIVE =  0;
     private static final int MEDIA_STATE_ACTIVE   =  1;
 
     private int mCurrentState = MEDIA_STATE_UNKNOWN;
+	
+	private static final String PLAY_STATE_CHANGED = "com.android.music.playstatechanged";
+	
+	private boolean shouldBePlaying = false;
+	private Handler handler = new Handler();
+
+	public MediaPlayPauseButton() {
+		mType = BUTTON_MEDIA_PLAY_PAUSE;
+	}
 
     @Override
     protected void updateState() {
@@ -31,7 +43,11 @@ public class MediaPlayPauseButton extends MediaKeyEventButton {
 
         mCurrentState = (isMusicActive() ? MEDIA_STATE_INACTIVE : MEDIA_STATE_ACTIVE);
 
-        update();
+		handler.postDelayed (new Runnable () {
+			public void run () {
+				update();
+			}
+		}, 20);
     }
 
     private boolean isMusicActive() {
@@ -41,6 +57,9 @@ public class MediaPlayPauseButton extends MediaKeyEventButton {
             if(am != null) {
                 mCurrentState = (am.isMusicActive() ? MEDIA_STATE_ACTIVE : MEDIA_STATE_INACTIVE);
             }
+			
+			if (shouldBePlaying)
+				return true;
 
             return (mCurrentState == MEDIA_STATE_ACTIVE);
         } else {
@@ -49,4 +68,26 @@ public class MediaPlayPauseButton extends MediaKeyEventButton {
             return active;
         }
     }
+
+	@Override
+	protected IntentFilter getBroadcastIntentFilter() {
+		IntentFilter iF = new IntentFilter();
+		iF.addAction(PLAY_STATE_CHANGED);
+		return iF;
+	}
+
+	@Override
+	protected void onReceive(Context context, Intent intent) {
+		String action = intent.getAction();
+		if (action.equals(PLAY_STATE_CHANGED)) {
+			if (intent.hasExtra("playing")) {
+				shouldBePlaying = intent.getBooleanExtra("playing", false);
+				handler.postDelayed (new Runnable () {
+					public void run () {
+						update();
+					}
+				}, 120);
+			}
+		}
+	}
 }
